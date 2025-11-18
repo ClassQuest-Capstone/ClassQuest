7bd5269afe8b0551396fb1bd84d01fa9e3f4ee59
TOLANI OKE-STEVE <emmasam1247@gamil.com>
2025-11-18 16:22:46 -0600

    frontend

---
+++ app/frontend/src/react-app-env.d.ts
@@ -0,0 +1,11 @@
+/// <reference path="../src/App.tsx" />
+/// <reference path="../src/index.tsx" />
+/// <reference path="../package-lock.json" />
+/// <reference path="../package.json" />
+
+declare namespace React {
+    export interface AppProps {
+        assetsBucketName: string;
+    }
+}
+declare const App: React.FC<React.AppProps>;
