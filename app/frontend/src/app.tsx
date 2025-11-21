6f537eeaf9acbadc86236065461a08e474e5f1a2
TOLANI OKE-STEVE <emmasam1247@gamil.com>
2025-11-21 11:54:34 -0600

    correctly renaming paths

--- app/frontend/src/app.tsx
+++ app/frontend/src/app.tsx
@@ -1,16 +1,16 @@
 import { Amplify } from "aws-amplify";
 //import awsExports from "./aws-exports";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import App from './home';
+import Home from './home';
 import { StudentLogin, TeacherLogin, Signup, Role} from './pages/auth';
 
 //Amplify.configure(awsExports); (Todo: enable when aws export is setup)
 
-export default function AppWrapper() {
+export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<Home />} />
         <Route path="/role" element={<Role />} />
         <Route path="/StudentLogin" element={<StudentLogin />} />
         <Route path="/TeacherLogin" element={<TeacherLogin />} />
