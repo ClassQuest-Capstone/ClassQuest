3e32eb51bb7f151ba48bb954908f4014c18d0c74
Alvin0428 <amt142@uregina.ca>
2025-11-24 17:15:28 -0600

    Add character equipment page, assets, and pet/background support

--- app/frontend/src/app.tsx
+++ app/frontend/src/app.tsx
@@ -1,12 +1,17 @@
 import { Amplify } from "aws-amplify";
-//import awsExports from "./aws-exports";
+// import awsExports from "./aws-exports";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import Home from './home';
-import { StudentLogin, TeacherLogin, Signup, Role} from './pages/auth';
+import { StudentLogin, TeacherLogin, Signup, Role } from './pages/auth';
+
 import StudentDashboard from './pages/students/studentDashboard';
 import TeacherDashboard from './pages/teacher/teacherDashboard';
 
-//Amplify.configure(awsExports); (Todo: enable when aws export is setup)
+// ⭐ NEW CHARACTER PAGE ⭐
+import CharacterPage from './pages/students/characterpage';
+
+// Amplify.configure(awsExports); // (Todo: enable when aws export is setup)
 
 export default function App() {
   return (
@@ -17,8 +22,13 @@ export default function App() {
         <Route path="/StudentLogin" element={<StudentLogin />} />
         <Route path="/TeacherLogin" element={<TeacherLogin />} />
         <Route path="/Signup" element={<Signup />} />
+        
+        {/* Dashboards */}
         <Route path="/StudentDashboard" element={<StudentDashboard />} />
         <Route path="/TeacherDashboard" element={<TeacherDashboard />} />
+
+        {/* ⭐ Character Page ⭐ */}
+        <Route path="/character" element={<CharacterPage />} />
       </Routes>
     </BrowserRouter>
   );
